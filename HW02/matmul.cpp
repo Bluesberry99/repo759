ed4298fbc8199047b6226380378efad80edd39f7
Bluesberry99 <117439355+Bluesberry99@users.noreply.github.com>
2024-10-04 14:39:24 -0500

    Update matmul.cpp

--- HW02/matmul.cpp
+++ HW02/matmul.cpp
@@ -38,7 +38,7 @@ void mmul3(double* A, double* B, double* C, int N) {
 void mmul4(std::vector<double>& A, std::vector<double>& B, std::vector<double>& C, int N) {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
- //           C[i * N + j] = 0; // Initialize C[i][j] to 0
+//           C[i * N + j] = 0; // Initialize C[i][j] to 0
             for (int k = 0; k < N; ++k) {
                 C[i * N + j] += A[i * N + k] * B[k * N + j]; // Row by column multiplication
             }
